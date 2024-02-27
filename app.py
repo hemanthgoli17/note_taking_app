f4a3ecd7cacea51e11aa0fbf11c488990f46e5d6
Hemanth Goli <hemanth17.goli@gmail.com>
2024-02-28 00:49:27 +0530

    Added Delete Functions and basic css

--- app.py
+++ app.py
@@ -14,9 +14,24 @@ def index():
     if request.method == "POST":
         note_text = request.form.get("note")
         if note_text:
-            notes.append({'text' : note_text})
-            save_notes()
+            notes.append(note_text)
+            save_notes(notes)
             return redirect(url_for('index'))
+        else:
+            return redirect(url_for('index'))
+
+@app.route('/delete_note/<note_text>', methods = ["POST"])
+def delete_note(note_text):
+    if request.method == "POST":
+        with open('notes.json', "r+") as f:
+            data = json.load(f)
+            for note_index,note_content in enumerate(data):
+                if note_content == note_text:
+                    del data[note_index]
+                    save_notes(data)
+                    break
+            f.seek(0)
+        return redirect(url_for('index'))
 
 def load_notes():
     global notes
@@ -26,9 +41,9 @@ def load_notes():
     except FileNotFoundError:
         notes = []
 
-def save_notes():
+def save_notes(data):
     with open('notes.json', 'w') as file:
-        json.dump(notes, file, indent=4)
+        json.dump(data, file, indent=4)
 
 if __name__ == '__main__':
     app.run(debug=True)
