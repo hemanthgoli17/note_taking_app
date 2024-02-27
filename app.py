33ea4bec221a31965b24a902502275a5f035eb75
Hemanth Goli <hemanth17.goli@gmail.com>
2024-02-27 16:29:43 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+import json
+
+app = Flask(__name__)
+
+notes = []
+
+@app.route('/')
+def index():
+    load_notes()
+    return render_template("home.html", notes = notes)
+
+@app.route('/add_note', methods = ["POST"])
+def add_note():
+    note_text = request.form["note"]
+    notes.append({'text' : note_text})
+    save_notes()
+    return index()
+
+def load_notes():
+    global notes
+    try:
+        with open('notes.json', 'r') as file:
+            notes = json.load(file)
+    except FileNotFoundError:
+        notes = []
+
+def save_notes():
+    with open('notes.json', 'w') as file:
+        json.dump(notes, file, indent=4)
+
+if __name__ == '__main__':
+    app.run(debug=True)
